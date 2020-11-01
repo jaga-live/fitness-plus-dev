0c1c749b11857d02f4392cd387a72f7521073c99
Jaga <58385390+jagadheesh6@users.noreply.github.com>
2020-11-01 21:15:26 +0530

    Update verify.auth.js

--- auth/verify.auth.js
+++ auth/verify.auth.js
@@ -1,27 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config()
+require("dotenv").config();
 
 //Database models
-const User = require('../models/user')
+const User = require("../models/user");
 
+module.exports = async (req, res, next) => {
+  try {
+    var token = req.headers.authorization.split(" ")[1];
 
+    var user = await User.countDocuments({ jwt: token });
+    if (!user) return res.status(403).send("Invalid Token");
 
-module.exports = async (req,res,next)=>{
+    var check = jwt.verify(token, process.env.JWT_SECRET);
 
-try{var token = (req.headers.authorization).split(" ")[1]
+    req.userData = check;
 
-var user = await User.countDocuments({'jwt':token})
-if(!user) return res.status(403).send('Invalid Token')
-
-var check = jwt.verify(token,process.env.JWT_SECRET)
-
-req.userData = check
-
-next()
-
-}catch{
-    return res.status(403).send("Unauthorized")
-}
-
-}
+    next();
+  } catch {
+    return res.status(403).send("Unauthorized");
+  }
+};
